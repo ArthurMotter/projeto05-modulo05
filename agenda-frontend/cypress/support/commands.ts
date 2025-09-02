23ab3eb438c445e41258156ed773dc1a73dc0437
Arthur Motter <arthur.motter@fatec.sp.gov.br>
2025-09-02 12:59:12 -0300

    feat(professional): e2e test

--- agenda-frontend/cypress/support/commands.ts
+++ agenda-frontend/cypress/support/commands.ts
@@ -1,37 +1,31 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+Cypress.Commands.add('getComponent', (selector) => {
+  return cy.get(selector).then($el => {
+    const win = $el[0].ownerDocument.defaultView;
+    // This is a type assertion to tell TypeScript that `ng` exists on the window
+    const ng = (win as any).ng; 
+    const el = $el[0];
+
+    const component = ng.getComponent(el);
+    if (!component) {
+      throw new Error(`Could not find a component for selector: ${selector}`);
+    }
+    return component;
+  });
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to get an Angular component instance from a DOM element.
+       * @example cy.getComponent('app-my-component')
+       */
+      getComponent(selector: string): Chainable<any>;
+    }
+  }
+}
+
+// THIS IS THE FIX: This line makes TypeScript treat this file as a module.
+export {};
